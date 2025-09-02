63e991fe37fbc224bf069f5c52d3857bfe8609f1
myoungho <lucky.myoungho@gmail.com>
2025-09-02 23:27:38 +0900

    test

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,6 +1,5 @@
-import { Button } from 'bits-ui';
-import { PostsList } from './components/PostsList';
-import { usePosts } from './hooks/usePosts';
+import { PostsList } from "./components/PostsList";
+import { usePosts } from "./hooks/usePosts";
 
 export default function App() {
   const posts = usePosts();
@@ -9,7 +8,6 @@ export default function App() {
     <div>
       <h1>게시판 (React + TS)</h1>
       <PostsList posts={posts} />
-      <Button>Bits UI 버튼</Button>
     </div>
   );
 }
