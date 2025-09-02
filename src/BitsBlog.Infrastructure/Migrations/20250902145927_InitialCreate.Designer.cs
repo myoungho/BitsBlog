3fe68f6f5be8063156e72758781dd2c2b0e170a1
myoungho <lucky.myoungho@gmail.com>
2025-09-03 00:01:32 +0900

    test

--- src/BitsBlog.Infrastructure/Migrations/20250902145927_InitialCreate.Designer.cs
+++ src/BitsBlog.Infrastructure/Migrations/20250902145927_InitialCreate.Designer.cs
@@ -4,6 +4,7 @@ using BitsBlog.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BitsBlog.Infrastructure.Migrations
 {
     [DbContext(typeof(BitsBlogDbContext))]
-    partial class BitsBlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250902145927_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
